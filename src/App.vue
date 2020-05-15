6bf892bc914d992c8e4f12d340e9c7da9a9867e7
ZoricmaTs <tszzzch@gmail.com>
2020-05-15 23:56:53 +0700

    add fonts

--- src/App.vue
+++ src/App.vue
@@ -6,5 +6,8 @@
 
 <style lang="scss">
 @import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap'); //основной
+@import url('https://fonts.googleapis.com/css2?family=Sacramento&display=swap'); //fitness
+@import url('https://fonts.googleapis.com/css2?family=Jura:wght@400;500&display=swap'); //онлайн
 @import '/assets/scss/main.scss';
 </style>
