7de19e2428d419a344d26f3ffd39bc95bab435bd
ZoricmaTs <tszzzch@gmail.com>
2020-05-17 23:22:20 +0700

    add footer, btn

---
+++ src/components/Footer.vue
@@ -0,0 +1,94 @@
+<template>
+  <footer class="footer-block footer-block__color_gray block-grid">
+    <div class="footer-block__logo">
+      <h1 class="logo__min">Fitness</h1>
+      <p class="footer-block__text">
+        Сервис по подбору тренера и тренировок. Занятия и общение в онлайн
+        режиме из любой точки планеты.
+      </p>
+      <a href="#" class="footer-block__link-email text__heading_size_m">fitness-online@fitness.com</a>
+    </div>
+    <div class="footer-block__menu">
+      <p class="footer-block__menu-heading">
+        Меню
+        <span class="footer-block__menu-heading_blue">навигации</span>:
+      </p>
+      <ul class="footer-block__menu-list">
+        <li class="footer-block__menu-item">
+          <a class="footer-block__menu-link" href="#">Регистрация</a>
+        </li>
+        <li class="footer-block__menu-item">
+          <a class="footer-block__menu-link" href="#">Вход</a>
+        </li>
+        <li class="footer-block__menu-item">
+          <a class="footer-block__menu-link" href="#">О сервисе</a>
+        </li>
+        <li class="footer-block__menu-item">
+          <a class="footer-block__menu-link" href="#">Направления</a>
+        </li>
+        <li class="footer-block__menu-item">
+          <a class="footer-block__menu-link" href="#">Вопросы и ответы</a>
+        </li>
+        <li class="footer-block__menu-item">
+          <a class="footer-block__menu-link" href="#">Контакты</a>
+        </li>
+      </ul>
+    </div>
+    <div class="footer-block__inst">
+      <p class="footer-block__menu-heading">Instagram</p>
+      <div class="footer-block__inst-img">
+        <img
+          class="footer-block__img"
+          src="../assets/img/inst1.png"
+          alt="instagram photo"
+          width="84"
+          height="84"
+        />
+        <img
+          class="footer-block__img"
+          src="../assets/img/inst2.png"
+          alt="instagram photo"
+          width="84"
+          height="84"
+        />
+        <img
+          class="footer-block__img"
+          src="../assets/img/inst3.png"
+          alt="instagram photo"
+          width="84"
+          height="84"
+        />
+        <img
+          class="footer-block__img"
+          src="../assets/img/inst4.png"
+          alt="instagram photo"
+          width="84"
+          height="84"
+        />
+        <img
+          class="footer-block__img"
+          src="../assets/img/inst5.png"
+          alt="instagram photo"
+          width="84"
+          height="84"
+        />
+        <img
+          class="footer-block__img"
+          src="../assets/img/inst6.png"
+          alt="instagram photo"
+          width="84"
+          height="84"
+        />
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+@import '../assets/scss/footer.scss';
+@import '../assets/scss/navbar.scss';
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,28 +4,68 @@
     <section class="title-img" id="title-home">
       <div class="title-img-block">
         <h1 class="logo__title">Fitness</h1>
-        <h3 class="text__heading_size_h3 text-title">
-          Нужно придумать какую-нибудь фразу в строку
-        </h3>
+        <h3 class="text__heading_size_h3 text-title">Нужно придумать какую-нибудь фразу в строку</h3>
         <div class="btn-block">
           <button
             class="btn__title btn__title_color_orange text__heading_size_h3"
             @click="showModal = !showModal"
-          >
-            Регистрация
-          </button>
+          >Регистрация</button>
           <button
-            class="btn__title btn__title_color_transp text__heading_size_h3 marg20"
+            class="btn__title title_color_transp-orange text__heading_size_h3 marg20"
             @click="showModalAuth = !showModalAuth"
-          >
-            Вход
-          </button>
+          >Вход</button>
         </div>
       </div>
     </section>
     <Homeoffers />
     <Homeaboutserv />
-    <Homeattainment />
+    <section class="attainment block-grid">
+      <h1
+        class="attainment__heading attainment__heading_color_white text__heading_size_h1"
+      >Что вы получаете?</h1>
+      <div class="attainment__blocks">
+        <div class="attainment__block">
+          <div class="attainment__img-block">
+            <img class="attainment__img" src="../assets/img/mat.svg" alt="comfortable training" />
+          </div>
+          <p class="text__heading_size_h3">Комфортные условия для тренировок</p>
+        </div>
+        <div class="attainment__block">
+          <div class="attainment__img-block">
+            <img class="attainment__img" src="../assets/img/yoga.svg" alt="training" />
+          </div>
+          <p class="text__heading_size_h3">Тренировки на любой вкус</p>
+        </div>
+        <div class="attainment__block">
+          <div class="attainment__img-block">
+            <img class="attainment__img" src="../assets/img/online.svg" alt="online training" />
+          </div>
+          <p class="text__heading_size_h3">Возможность проводить тренировки онлайн</p>
+        </div>
+      </div>
+    </section>
+    <div class="attainment__register block-grid">
+      <span
+        class="attainment__heading attainment__heading_color_gray text__heading_size_h1"
+      >Готовы приступить</span>
+      <span class="attainment__heading attainment__heading_color_white text__heading_size_h1">_</span>
+      <span
+        class="attainment__heading attainment__heading_color_blue text__heading_size_h1"
+      >тренировкам</span>
+      <span class="attainment__heading attainment__heading_color_gray text__heading_size_h1">?</span>
+      <div class="btn-block block-grid">
+        <button
+          class="btn__title btn__title_color_blue text__heading_size_h3"
+          @click="showModal = !showModal"
+        >Регистрация</button>
+        <button
+          class="btn__title title_color_transp-blue text__heading_size_h3 marg20"
+          @click="showModalAuth = !showModalAuth"
+        >Вход</button>
+      </div>
+    </div>
+
+    <Footer />
     <RegisterForm
       v-if="showModal"
       @registration="onRegister"
@@ -59,7 +99,7 @@ import NotConfirmed from '@/components/NotConfirmed.vue'
 import Navbar from '@/components/Navbar.vue'
 import Homeoffers from '@/components/Homeoffers.vue'
 import Homeaboutserv from '@/components/Homeaboutserv.vue'
-import Homeattainment from '@/components/Homeattainment.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   components: {
@@ -70,7 +110,7 @@ export default {
     Navbar,
     Homeoffers,
     Homeaboutserv,
-    Homeattainment
+    Footer
   },
   data() {
     return {
@@ -106,6 +146,7 @@ export default {
 @import '../assets/scss/home-offers.scss';
 @import '../assets/scss/home-aboutserv.scss';
 @import '../assets/scss/home-attainment.scss';
+@import '../assets/scss/footer.scss';
 
 .block-grid {
   padding: 0 25px;
